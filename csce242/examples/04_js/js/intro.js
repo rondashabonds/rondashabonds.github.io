a5f38ec971493c82ed3a67fd241deeeccc999b60
Rondasha Bonds <rondashabonds@gmail.com>
2025-09-09 12:51:44 -0400

    updated

---
+++ csce242/examples/04_js/js/intro.js
@@ -0,0 +1,13 @@
+/*
+const sayHello =() =>{
+    console.log("Hello World!");
+
+}
+
+  document.getElementById("btn-click-me").onclick= sayHello();
+
+*/
+
+document.getElemnetId("btn-click-me").onclick =() => {
+
+};
