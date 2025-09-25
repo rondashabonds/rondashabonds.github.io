3149b12b3d98881a69cf5b6ebcdcf320c72bbad4
Rondasha Bonds <rondashabonds@gmail.com>
2025-09-25 12:29:20 -0400

    updated

--- csce242/examples/04_js/js/intro.js
+++ csce242/examples/04_js/js/intro.js
@@ -1,14 +1,132 @@
 /*
-const sayHello =() =>{
-    console.log("Hello World!");
-
+const sayHello = () => {
+    console.log("Hello World");
 }
 
-  document.getElementById("btn-click-me").onclick= sayHello();
-
+document.getElementById("btn-click-me").onclick = sayHello;
 */
 
-document.getElemnetId("btn-click-me").onclick =() => {
-    console.log("helloworld")
-
+document.getElementById("btn-click-me").onclick = (event) => {
+    document.getElementById("p-welcome").innerHTML = "Hello World";
+    event.currentTarget.classList.add("clicked");   //current target is the button that was clicked
 };
+
+document.getElementById("btn-happy").onclick = () => {
+    const pFeeling = document.getElementById("p-feeling");
+    pFeeling.innerHTML = "YAY";
+    pFeeling.classList.add("happy");
+    pFeeling.classList.remove("sad");
+}
+
+document.getElementById("btn-sad").onclick = () => {
+    const pFeeling = document.getElementById("p-feeling");
+    pFeeling.innerHTML = "Nay";
+    pFeeling.classList.add("sad");
+    pFeeling.classList.remove("happy");
+}
+
+document.getElementById("btn-clear").onclick = () => {
+    const pFeeling = document.getElementById("p-feeling");
+    pFeeling.innerHTML = "";
+    pFeeling.classList.remove("sad");
+    pFeeling.classList.remove("happy");
+}
+
+document.getElementById("txt-emotion").onkeyup = (event) => {
+    const userInput = event.currentTarget.value;
+    document.getElementById("p-emotion").innerHTML = `You are feeling ${userInput}.`
+    document.getElementById("img-emotion").classList.remove("hidden");
+};
+
+document.getElementById("btn-mood-ring").onclick = () => {
+    const color = document.getElementById("txt-color").value.trim().toLowerCase();
+    const p = document.getElementById("p-mood-result");
+    p.innerHTML = "";   //reset paragraph
+    const error = document.getElementById("error-color");
+    error.innerHTML = "";  //reset error
+    let mood = "";
+    
+    if(color == ""){
+        error.innerHTML = "* blank";
+        return;
+    }
+
+    if(color == "red") {
+        mood = "angry";
+    } else if(color == "yellow") {
+        mood = "mellow";
+    }
+
+    if(mood == ""){
+        error.innerHTML = "* Invalid color";
+        return;
+    }
+
+    p.innerHTML = `You choose ${color}, so you are feeling ${mood}`;
+}
+
+document.getElementById("btn-bounce").onclick = (event) => {
+    const ball = document.getElementById("ball");
+
+    if(ball.classList.contains("bounce")){
+        event.currentTarget.innerHTML = "Start";
+    } else {
+        event.currentTarget.innerHTML = "Stop";
+    }
+
+    ball.classList.toggle("bounce");
+}
+
+/* Counter */
+let counter = 0;
+let counterInterval;
+const countP = document.getElementById("p-count");
+const btnStartCount = document.getElementById("btn-count-start");
+const btnPauseCount = document.getElementById("btn-count-pause")
+btnPauseCount.disabled = true;
+
+btnStartCount.onclick = () => {
+    btnStartCount.disabled = true;
+    btnPauseCount.disabled = false;
+    counterInterval = setInterval(()=>{
+        counter++;
+        countP.innerHTML = counter;
+    }, 1000);
+}
+
+btnPauseCount.onclick = () => {
+    clearInterval(counterInterval);
+    btnPauseCount.disabled = true;
+    btnStartCount.disabled = false;
+}
+
+/* Donation stuff */
+const goal = 10000;
+let totalDonations = 0;
+document.getElementById("goal-span").innerHTML = goal;
+
+document.getElementById("btn-donations").onclick = () => {
+    const donation = document.getElementById("txt-donations").value;
+    const errorSpan = document.getElementById("donation-error");
+    errorSpan.innerHTML = "";
+    const donationMessage = document.getElementById("donation-message");
+    donationMessage.innerHTML = "";
+
+    if(isNaN(donation) || donation <= 0){
+        errorSpan.innerHTML = "* Invalid Amount."
+        return;
+    }
+
+    totalDonations += parseInt(donation);
+    const donationPercent = totalDonations / goal * 100;
+
+    if(donationPercent >= 100){
+        donationMessage.innerHTML = "Goal Reached!";
+    } else if(donationPercent >= 50) {
+        donationMessage.innerHTML = "Over Half Way There";
+    } else {
+        donationMessage.innerHTML = "Let's get Goaling";
+    }
+
+    document.querySelector(":root").style.setProperty("--donation-percent", donationPercent + "%");
+}
---
+++ csce242/examples/04_js/js/loops.js
@@ -0,0 +1,60 @@
+document.getElementById("btn-display").onclick = () => {
+    const displaySection = document.getElementById("loop-result");
+    const ul = document.createElement("ul");
+    displaySection.append(ul);
+
+    //loop and write the numbers one through 10
+    for(let i=0; i < 10; i++){
+        const li = document.createElement("li");
+        li.innerHTML = `${i} banana(s)`;
+        ul.append(li);
+    }
+};
+
+document.getElementById("btn-count").onclick = () => {
+    const error = document.getElementById("error-number");
+    error.innerHTML = "";
+
+    const startNum = parseInt(document.getElementById("txt-start").value);
+    const endNum = parseInt(document.getElementById("txt-end").value);
+
+    const resultDiv = document.getElementById("d-count");
+    resultDiv.innerHTML = "";
+
+    if(startNum > endNum) {
+        error.innerHTML = "* The second number must be bigger than the first";
+        return;
+    }
+
+    //Loop for ever number from start to end display the number in paragraphs
+    for(let i=startNum; i <= endNum; i++){
+        const p = document.createElement("p");
+        p.innerHTML = i;
+        resultDiv.append(p);
+        p.onclick = () => {
+            console.log(`You clicked Number ${i}`);
+        };
+    }
+};
+
+document.getElementById("btn-show-toys").onclick = () => {
+    const toyList = document.getElementById("toy-list");
+    toyList.innerHTML = "";
+
+    const toys = ["ball", "skipping rope", "doll", "mini car", "elmo"];
+
+    /* First way to iterate */
+    //interate over toys  add li's to the ul
+   /* for(let i=0; i < toys.length; i++){
+        const li = document.createElement("li");
+        li.innerHTML = toys[i];
+        toyList.append(li);
+    }*/
+
+    /* Second way preferred */
+    toys.forEach((toy, i)=>{
+        const li = document.createElement("li");
+        li.innerHTML = toy;
+        toyList.append(li);
+    });
+}
